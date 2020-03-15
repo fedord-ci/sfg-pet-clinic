4cb722ff539ff3906bb249fffb301b0884cfb6e6
FedorD <fedord.dev@gmail.com>
2020-03-15 15:16:03 +0700

    Add index controller

---
+++ pet-clinic-web/src/main/java/pw/springdev/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package pw.springdev.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by FedorD on 2020-03-15
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
