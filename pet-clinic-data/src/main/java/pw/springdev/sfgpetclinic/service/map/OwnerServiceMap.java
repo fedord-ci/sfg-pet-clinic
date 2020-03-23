9b6b325ce89eecf944c0820c7db33f948a526325
FedorD <fedord.dev@gmail.com>
2020-03-23 20:00:16 +0700

    Adding Spring Config. Closes #22

--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/service/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/service/map/OwnerServiceMap.java
@@ -1,5 +1,6 @@
 package pw.springdev.sfgpetclinic.service.map;
 
+import org.springframework.stereotype.Service;
 import pw.springdev.sfgpetclinic.model.Owner;
 import pw.springdev.sfgpetclinic.service.OwnerService;
 
@@ -8,6 +9,7 @@ import java.util.Set;
 /**
  * Created by FedorD on 2020-03-15
  */
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long>
     implements OwnerService {
     @Override
--- pet-clinic-web/src/main/java/pw/springdev/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pw/springdev/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,17 +1,11 @@
 package pw.springdev.sfgpetclinic.bootstrap;
 
-import java.time.LocalDate;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pw.springdev.sfgpetclinic.model.Owner;
-import pw.springdev.sfgpetclinic.model.Pet;
-import pw.springdev.sfgpetclinic.model.PetType;
 import pw.springdev.sfgpetclinic.model.Vet;
 import pw.springdev.sfgpetclinic.service.OwnerService;
 import pw.springdev.sfgpetclinic.service.VetService;
-import pw.springdev.sfgpetclinic.service.map.OwnerServiceMap;
-import pw.springdev.sfgpetclinic.service.map.VetServiceMap;
 
 /**
  * Created by FedorD on 2020-03-23
@@ -22,9 +16,10 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
