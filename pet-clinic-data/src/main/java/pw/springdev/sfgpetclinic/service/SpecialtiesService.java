2db0accefbba4ebf2a9af290fa22ae512c5ef4ac
FedorD <fedord.dev@gmail.com>
2020-03-26 13:29:08 +0700

    Added Specialties Servise Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/service/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package pw.springdev.sfgpetclinic.service;
+
+import pw.springdev.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by FedorD on 2020-03-26
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
