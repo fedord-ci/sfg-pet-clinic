85b7fff77e641616607cfe76834e12a159c99223
FedorD <fedord.dev@gmail.com>
2020-03-26 10:46:29 +0700

    Added pet type service. Closes #29

---
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,9 @@
+package pw.springdev.sfgpetclinic.service;
+
+import pw.springdev.sfgpetclinic.model.PetType;
+
+/**
+ * Created by FedorD on 2020-03-26
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
