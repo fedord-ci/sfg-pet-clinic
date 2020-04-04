af71955b9b37e59541db08d8518d15e9a04866a3
FedorD <fedord.dev@gmail.com>
2020-04-04 22:48:26 +0700

    Update vet to use JPA. Closes #37

--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Vet.java
@@ -1,13 +1,20 @@
 package pw.springdev.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by FedorD on 2020-03-14
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    // EAGER means Spring loads specialities when loads vet
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
