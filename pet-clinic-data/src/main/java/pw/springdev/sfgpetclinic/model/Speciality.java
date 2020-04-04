d2cb8f97869be6ccd0a2b5c4e5a6cb1af9be99e9
FedorD <fedord.dev@gmail.com>
2020-04-04 22:48:26 +0700

    Update vet to use JPA. Closes #37

--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package pw.springdev.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by FedorD on 2020-03-26
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
