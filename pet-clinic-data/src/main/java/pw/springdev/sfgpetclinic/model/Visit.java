8dbc521e29979fffaa25b1e8f72240f0757d4772
FedorD <fedord.dev@gmail.com>
2020-04-04 22:57:05 +0700

    Add Visit Entity. Close #38

--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Visit.java
@@ -1,14 +1,32 @@
 package pw.springdev.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by FedorD on 2020-03-26
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
 
     public LocalDate getDate() {
         return date;
