656490012cae2b3002c5829ee33166e51ab4c39a
FedorD <fedord.dev@gmail.com>
2020-04-04 22:28:48 +0700

    Update owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Owner.java
@@ -1,16 +1,27 @@
 package pw.springdev.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by FedorD on 2020-03-14
  */
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    // if an owner will be deleted then it will be deleted pets cascaded
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Person.java
@@ -1,10 +1,18 @@
 package pw.springdev.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by FedorD on 2020-03-14
  */
+@MappedSuperclass
 public class Person extends BaseEntity {
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
