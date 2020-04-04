5b927dea1322a06f756beab3a0fd701ee539b936
FedorD <fedord.dev@gmail.com>
2020-04-04 22:28:48 +0700

    Update owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Pet.java
@@ -1,15 +1,27 @@
 package pw.springdev.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by FedorD on 2020-03-14
  */
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/PetType.java
@@ -1,9 +1,17 @@
 package pw.springdev.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by FedorD on 2020-03-14
  */
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
