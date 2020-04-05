277acaa491dc791e432cdd916fa90aa5ec143684
FedorD <fedord.dev@gmail.com>
2020-04-05 22:49:21 +0700

    Add Project Lombok to project. Closes #51

--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/Person.java
@@ -1,6 +1,9 @@
 package pw.springdev.sfgpetclinic.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
@@ -10,8 +13,16 @@ import javax.persistence.MappedSuperclass;
  */
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     @Column(name = "first_name")
     private String firstName;
