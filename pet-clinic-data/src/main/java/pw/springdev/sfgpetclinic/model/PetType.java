bfb976843078f364de091cb9adfa9ec91a0410cd
FedorD <fedord.dev@gmail.com>
2020-04-05 22:28:21 +0700

    Add Lombok to model package

--- pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pw/springdev/sfgpetclinic/model/PetType.java
@@ -1,5 +1,7 @@
 package pw.springdev.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -7,18 +9,15 @@ import javax.persistence.Table;
 /**
  * Created by FedorD on 2020-03-14
  */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
