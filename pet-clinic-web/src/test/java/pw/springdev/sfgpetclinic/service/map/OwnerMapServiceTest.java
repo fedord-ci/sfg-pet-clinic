4be593f02f1200bafbf213eae1f38c7095ace991
Fedor <fdenisov137@gmail.com>
2020-04-07 12:37:19 +0700

    Add service, fix gitignore

---
+++ pet-clinic-web/src/test/java/pw/springdev/sfgpetclinic/service/map/OwnerMapServiceTest.java
@@ -0,0 +1,37 @@
+package pw.springdev.sfgpetclinic.service.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
